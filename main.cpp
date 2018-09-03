14388913e797f9eb10ce4c664adc3e4aeed174d0
xiaoshi <31793188+muyangdaoren@users.noreply.github.com>
2018-09-03 17:21:38 +0800

    3
    3

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@
 int CALLBACK _tWinMain(HINSTANCE hInstance,HINSTANCE hPrevInst,LPTSTR lpCmdLine,int nShowCmd)
 {
 
-
+    for(int i = 0; i < 100; i++)
+    {
+     std::cout<<"hello world!"<<std::end;   
+    }
     CELLWinApp  app(hInstance);
 
     app.main(800,600);
